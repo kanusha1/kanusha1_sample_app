2e0dd7592da9351eb40754516729a417281f0bdc
kanusha1 <kanusha1.email@gmail.com>
2014-09-13 22:32:09 -0400

    Changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleApp::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
+  root 'home#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
